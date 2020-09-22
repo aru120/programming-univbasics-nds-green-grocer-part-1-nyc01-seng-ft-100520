a755555d8c4e78154eff8c46652640ff9ac6fcf2
Kevin Huang <khuang159@gmail.com>
2020-09-22 19:21:33 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -32,7 +32,7 @@ final = find_item_by_name_in_collection(key.values[0],newHash)
   if final == nil
     newHash << key
     newHash[i][:count] = 1
-   binding.pry
+   
   else 
   newHash[i][:count] + 1
   

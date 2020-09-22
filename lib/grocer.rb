e971d1a4bc2b598db6ad385eb1949c06fac14f71
Kevin Huang <khuang159@gmail.com>
2020-09-22 18:19:57 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,17 +1,52 @@
-def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+require 'pry'
 
+
+grocery_shelf = [
+  { :item => "CANNED BEANS", :price => 3.00, :clearance => true },
+  { :item => "CANNED CORN", :price => 2.50, :clearance => false },
+  { :item => "SALSA", :price => 1.50, :clearance => false },
+  { :item => "TORTILLAS", :price => 2.00, :clearance => false },
+  { :item => "HOT SAUCE", :price => 1.75, :clearance => false }
+]
+
+
+def find_item_by_name_in_collection(name, collection)
+  
+   result = nil
+  collection.each do |key|
+   
+    if key[:item] == name
+      result = key
+    end
 end
+   return result
+end
+
+
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  newHash ={}
+cart.each_with_index do |key|
 
+  key.each do |inner_k, inner_v|
+    
+    find_item_by_name_in_collection(inner_v,cart)
+    binding.pry
+  # newHash[inner_k] = inner_v
+  # newHash[:count] = 1
+  # binding.pry
+  # if newHash[inner_k] = inner_v
+  #   newHash[:count] += 1
+  # end
+    
+end
 end
 
+  newHash
+
+ 
+end
+
+#consolidate_cart(grocery_shelf)
 
   

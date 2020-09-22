23abda8595afa147fccf78bfd54fb4143820da58
Kevin Huang <khuang159@gmail.com>
2020-09-22 19:12:12 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -31,7 +31,7 @@ cart.each_with_index do |key|
 final = find_item_by_name_in_collection(key.values[0],newHash)
   if final == nil
     newHash << key
-    newHash[0][:count] = 1
+    newHash[i][:count] = 1
    binding.pry
   else
   newHash[:count] += 1
